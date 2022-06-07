736198f43a6239605304c30b9bf3eff77aa75381
Vilhelmina <90849312+vilmelhina@users.noreply.github.com>
2022-06-07 19:59:20 +0200

    More explaining printouts from main method

--- src/main/java/vilhelmina/hgtask2/Main.java
+++ src/main/java/vilhelmina/hgtask2/Main.java
@@ -9,19 +9,29 @@ public class Main {
         
         System.out.println("-------------- INHERITANCE --------------\n");
         
+        System.out.println("Instance of InheritanceDay is created...");
         InheritanceDay inheritanceDay = new InheritanceDay(2022,06,06);
+        System.out.println("First task is added...");
         inheritanceDay.addTask(firstTask);
+        System.out.println("Second task is added...");
         inheritanceDay.addTask(secondTask);
+        System.out.println("Completion status of first task is set to true...");
         inheritanceDay.setCompleted(firstTask, true);
+        System.out.println("Instance of InheritanceDay is printed...\n");
         System.out.println(inheritanceDay);
         
         
         System.out.println("\n-------------- COMPOSITION --------------\n");
         
+        System.out.println("Instance of CompositionDay is created...");
         CompositionDay compositionDay = new CompositionDay(2022,06,06);
+        System.out.println("First task is added...");
         compositionDay.addTask(firstTask);
+        System.out.println("Second task is added...");
         compositionDay.addTask(secondTask);
+        System.out.println("Completion status of first task is set to true...");
         compositionDay.setCompleted(firstTask, true);
+        System.out.println("Instance of CompositionDay is printed...\n");
         System.out.println(compositionDay);
         
     }
